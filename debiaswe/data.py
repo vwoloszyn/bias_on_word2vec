ac137a287abe4d0327bcc50a4c0ef7a7c73c44eb
brendasalenave <brendasalenave@live.com>
2018-04-27 13:32:02 -0300

    accuracy test

--- debiaswe/data.py
+++ debiaswe/data.py
@@ -11,17 +11,6 @@ Tolga Bolukbasi, Kai-Wei Chang, James Zou, Venkatesh Saligrama, and Adam Kalai
 PKG_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def load_professions():
-    professions_file = os.path.join(PKG_DIR, '../data', 'professions.json')
-    with open(professions_file, 'r') as f:
-        professions = json.load(f)
-    print('Loaded professions\n' +
-          'Format:\n' +
-          'word,\n' +
-          'definitional female -1.0 -> definitional male 1.0\n' +
-          'stereotypical female -1.0 -> stereotypical male 1.0')
-    return professions
-
 def load_professions_pt():
     professions_file = os.path.join(PKG_DIR, '../data', 'professions_pt.json')
     with open(professions_file, 'r') as f:
